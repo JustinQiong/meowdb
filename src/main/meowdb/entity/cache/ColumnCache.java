80c0ea996a66c1a5ceb78567f501b44f4208f8cb
JustinQiong <Justin.qiong@gmail.com>
2021-11-25 15:20:54 +0800

    Create ColumnCache.java

---
+++ src/main/meowdb/entity/cache/ColumnCache.java
@@ -0,0 +1,13 @@
+package meowdb.entity.cache;
+
+/**
+ * 列缓存
+ *
+ * @author justin.qiong@outlook.com
+ * @since 2021/2/23 9:29
+ */
+public class ColumnCache extends CacheBase {
+    public ColumnCache(String key) {
+        super(key);
+    }
+}
