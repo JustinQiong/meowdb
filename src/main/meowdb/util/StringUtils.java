490888d62fe48e7c5787fa451009186ad5700021
JustinQiong <Justin.qiong@gmail.com>
2021-11-25 15:30:31 +0800

    Create StringUtils.java

---
+++ src/main/meowdb/util/StringUtils.java
@@ -0,0 +1,12 @@
+package meowdb.util;
+
+/**
+ * @author justin.qiong@outlook.com
+ * @since 2021/2/18 17:28
+ */
+public class StringUtils {
+
+    public static boolean isEmpty(String s) {
+        return s == null || s.equals("");
+    }
+}
