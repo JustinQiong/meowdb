7d5433f44b49dc76d06d6d3f22a6bc13fc344483
JustinQiong <Justin.qiong@gmail.com>
2021-11-25 15:14:27 +0800

    Create SqlStyler.java

---
+++ src/main/meowdb/boot/SqlStyler.java
@@ -0,0 +1,88 @@
+package meowdb.boot;
+
+import javafx.application.Platform;
+import meowdb.common.Keywords;
+import org.fxmisc.richtext.GenericStyledArea;
+import org.fxmisc.richtext.model.Paragraph;
+import org.fxmisc.richtext.model.StyleSpans;
+import org.fxmisc.richtext.model.StyleSpansBuilder;
+import org.reactfx.collection.ListModification;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author justin.qiong@outlook.com
+ * @since 2021/3/1 14:19
+ */
+public class SqlStyler<PS, SEG, S> implements Consumer<ListModification<? extends Paragraph<PS, SEG, S>>> {
+    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", Keywords.all()) + ")\\b";
+    private static final String PAREN_PATTERN = "\\(|\\)";
+    private static final String BRACE_PATTERN = "\\{|\\}";
+    private static final String BRACKET_PATTERN = "\\[|\\]";
+    private static final String SEMICOLON_PATTERN = "\\;";
+    private static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*?\"|'([^\"\\\\]|\\\\.)*?'";
+    private static final String OPERATOR_PATTERN = "[=<>!]";
+    private static final String COMMENT_PATTERN = "/\\*[^\n]*" + "|" + "/\\*(.|\\R)*?\\*/"   // for whole text processing (text blocks)
+            + "|" + "/\\*[^\\v]*" + "|" + "^\\h*\\*([^\\v]*|/)";  // for visible paragraph processing (line by line)
+    private static final Pattern PATTERN = Pattern.compile(
+            "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
+                    + "|(?<PAREN>" + PAREN_PATTERN + ")"
+                    + "|(?<BRACE>" + BRACE_PATTERN + ")"
+                    + "|(?<BRACKET>" + BRACKET_PATTERN + ")"
+                    + "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
+                    + "|(?<STRING>" + STRING_PATTERN + ")"
+                    + "|(?<OPERATOR>" + OPERATOR_PATTERN + ")"
+                    + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
+    );
+    private final GenericStyledArea<PS, SEG, S> area;
+    private final Function<String, StyleSpans<S>> computeStyles;
+    private int prevParagraph, prevTextLength;
+
+    public SqlStyler(GenericStyledArea<PS, SEG, S> area, Function<String, StyleSpans<S>> computeStyles) {
+        this.computeStyles = computeStyles;
+        this.area = area;
+    }
+
+    @Override
+    public void accept(ListModification<? extends Paragraph<PS, SEG, S>> lm) {
+        if (lm.getAddedSize() > 0) {
+            int paragraph = Math.min(area.firstVisibleParToAllParIndex() + lm.getFrom(), area.getParagraphs().size() - 1);
+            String text = area.getText(paragraph, 0, paragraph, area.getParagraphLength(paragraph));
+
+            if (paragraph != prevParagraph || text.length() != prevTextLength) {
+                int startPos = area.getAbsolutePosition(paragraph, 0);
+                Platform.runLater(() -> area.setStyleSpans(startPos, computeStyles.apply(text)));
+                prevTextLength = text.length();
+                prevParagraph = paragraph;
+            }
+        }
+    }
+
+    public static StyleSpans<Collection<String>> computeHighlighting(String text) {
+        Matcher matcher = PATTERN.matcher(text);
+        int lastKwEnd = 0;
+        StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
+        while (matcher.find()) {
+            String styleClass =
+                    matcher.group("KEYWORD") != null ? "keyword" :
+                            matcher.group("PAREN") != null ? "paren" :
+                                    matcher.group("BRACE") != null ? "brace" :
+                                            matcher.group("BRACKET") != null ? "bracket" :
+                                                    matcher.group("SEMICOLON") != null ? "semicolon" :
+                                                            matcher.group("STRING") != null ? "string" :
+                                                                    matcher.group("OPERATOR") != null ? "operator" :
+                                                                            matcher.group("COMMENT") != null ? "comment" : null;
+            assert styleClass != null;
+            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
+            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
+            lastKwEnd = matcher.end();
+        }
+        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
+        return spansBuilder.create();
+    }
+}
