8eb6afebfe32270fa0dcd4c1d1b271a11a1ba930
JustinQiong <Justin.qiong@gmail.com>
2022-03-24 11:22:32 +0800

    Create FileLoader.java

---
+++ src/main/java/meowdb/util/FileLoader.java
@@ -0,0 +1,24 @@
+package meowdb.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileLoader {
+
+    /**
+     * 按行加载文件
+     */
+    public static List<String> load(String path) throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            return lines;
+        }
+    }
+}
